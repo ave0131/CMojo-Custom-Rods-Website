9c825ffcbfd5e0ecf0270d833740e3f79f6ad2de
ave0131 <mjols020@crk.umn.edu>
2026-05-13 00:53:46 -0500

    Added an image of a frozen lake at sunset as a temporary placeholder on the home page.

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 // This is a page for the home route of the website.
+import Image from 'next/image'
 export default function Home() {
   return (
     <main className="bg-white min-h-screen">
@@ -17,6 +18,16 @@ export default function Home() {
         <button className="mt-8 bg-slate-900 text-white px-8 py-3 rounded-md font-medium hover:bg-slate-700 transition-all">
           Build Your Rod
         </button>
+
+        {/* Image of a frozen lake at sunset TEMP */}
+        <div className="relative mt-14 w-[500px] h-[300px] mx-auto">
+          <Image
+            src="/images/FrozenLakeSunset.png"
+            alt="A frozen lake at sunset"
+            fill
+            className="object-cover rounded-lg"
+          />
+        </div>
 
       </section>
     </main>
